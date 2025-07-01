7c5335bb902ea4ec142509db6ff451c9e1ad7692
BatDev64 <bat.bits64@gmail.com>
2025-07-01 13:10:35 -0300

    fix: remove log

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const isProd = process.env.NODE_ENV === 'production';
 const isVercelProd = process.env.VERCEL_ENV === 'production';
 
-console.log(!(isVercelProd && isProd) ? 'dev' : 'prod')
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
