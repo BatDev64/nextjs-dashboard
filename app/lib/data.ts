3e6bd02879cd9086acc0a080740f0368657a80a8
BatDev64 <bat.bits64@gmail.com>
2025-07-02 11:18:53 -0300

    feat: add neon data base

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,6 @@
-import postgres from 'postgres';
+// import postgres from 'postgres';
+import { neon } from "@neondatabase/serverless";
+
 import {
   CustomerField,
   CustomersTableType,
@@ -9,7 +11,8 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = neon(`${process.env.DATABASE_URL}`);
 
 export async function fetchRevenue() {
   try {
